a514db8ab80a0911b6ad7230b0fa6e18f38a8a5a
Sylvia <sylviachebet03@gmail.com>
2024-03-13 00:29:45 +0300

    add

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"]
 beforeEach(function () {
-    
+
     cats.length = 0;
-  
+
     cats.push("Milo", "Otis", "Garfield");
-  });
-  function destructivelyAppendCat(name = "Ralph"){
+});
+function destructivelyAppendCat(name = "Ralph") {
     cats.push("Ralph")
-  }
-function destructivelyPrependCat(name = "Bob"){
+}
+function destructivelyPrependCat(name = "Bob") {
     cats.unshift("Bob")
 }
-function destructivelyRemoveLastCat(name = "Garfield"){
+function destructivelyRemoveLastCat(name = "Garfield") {
     cats.pop("Garfield")
 }
-function destructivelyRemoveFirstCat(name = "Milo"){
+function destructivelyRemoveFirstCat(name = "Milo") {
     cats.shift("Milo")
 }
-function appendCat(name = "Broom"){
-    return[...cats, "Broom"]
+function appendCat(name = "Broom") {
+    return [...cats, "Broom"]
 }
-function prependCat(name = "Arnold"){
-    return["Arnold", ...cats]
+function prependCat(name = "Arnold") {
+    return ["Arnold", ...cats]
 }
-function removeLastCat(){
-    return cats.slice(0,-1);
+function removeLastCat() {
+    return cats.slice(0, -1);
 }
-function removeFirstCat(){
+function removeFirstCat() {
     return cats.slice(1)
 }
